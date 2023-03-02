71e85315ba2b197a6dd28a50335a905557b56bed
xieb03 <264531057@qq.com>
2023-03-03 00:53:09 +0800

    283

--- simple/283.move-zeroes.py
+++ simple/283.move-zeroes.py
@@ -48,7 +48,7 @@ class Solution_2:
 
 
 # noinspection PyPep8Naming,PyMethodMayBeStatic
-class Solution:
+class Solution_3:
     # 只需要一次遍历。即在交换的中就填充0，不需要最后重新填一遍
     def moveZeroes(self, nums: List[int]) -> None:
         """
@@ -64,8 +64,36 @@ class Solution:
                 current_index += 1
 
 
+# noinspection PyPep8Naming,PyMethodMayBeStatic
+class Solution:
+    # 只需要一次遍历。即在交换的中就填充0，不需要最后重新填一遍
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # 找到第一个 0 的位置，默认会被下面改变，不改变表示没有 0
+        current_index = -1
+        for i, value in enumerate(nums):
+            if value == 0:
+                # 当前可以填充元素的位置
+                current_index = i
+                break
+        # 如果 current_index 没有改变，说明没有 0，不需要操作
+        if current_index == -1:
+            return
+        # 快排的思想，找到锚点 0，然后将满足条件的移到锚点的左侧，不满足锚点的移到锚点的右侧
+        for i in range(current_index + 1, len(nums)):
+            # 注意不能使用 enumerate 生成器，因为后面会改变 nums 中的元素，会影响生成器
+            # for i, value in enumerate(nums[current_index + 1:]):
+            value = nums[i]
+            if value != 0:
+                # 这里可以采用 Solution_2 的方式直接赋 0，因为已经回避了中索引相同的问题，此时 i 一定 领先于 current_index
+                nums[current_index], nums[i] = value, 0
+                current_index += 1
+
+
 def main():
-    for clazz in (Solution, Solution_1, Solution_2):
+    for clazz in (Solution, Solution_1, Solution_2, Solution_3):
         solution = clazz()
 
         a_list = [0, 1, 0, 3, 12]
@@ -79,6 +107,10 @@ def main():
         a_list = [1]
         solution.moveZeroes(a_list)
         assert a_list == [1]
+
+        a_list = [2, 1]
+        solution.moveZeroes(a_list)
+        assert a_list == [2, 1]
 
 
 if __name__ == '__main__':
